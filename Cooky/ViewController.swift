e7a5580c95ae3bf2e75bd63a94f743dd73403083
Aslan <shamkhanislam00@gmail.com>
2022-07-15 01:24:28 +0500

    Initial Commit

---
+++ Cooky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cooky
+//
+//  Created by Aslan Murat on 15.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
